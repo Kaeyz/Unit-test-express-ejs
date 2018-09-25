8044747ae4e6a31799b9b9ce06fc60f8e4a45f8d
kaeyz <kolagbele@gmail.com>
2018-09-25 14:21:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/home', (req, res) => {
+    res.render('home', {
+        message: 'Welcome Home Dude!'
+    })
+})
+
+module.exports = app
